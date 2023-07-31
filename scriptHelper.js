405610f8f4bd442c2e563d6f55fae7a85cfe9132
Timothy Nicolas <timnicolas87@gmail.com>
2023-07-31 18:25:08 -0500

    second commit

--- scriptHelper.js
+++ scriptHelper.js
@@ -46,26 +46,29 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         let fuelStatus = document.getElementById("fuelStatus");
         let cargoStatus = document.getElementById("cargoStatus");
         
-        
-        if (fuelLevel.value < 10000 || cargoLevel.value > 10000 && validateFuel === "Is a Number" || validateCargo === "Is a Number" ) {
-            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = "#C7254E";
-            list.style.visibility = "visible";
-            if (fuelLevel.value < 10000) {
-                fuelStatus.innerHTML = "Fuel level too low for launch";
-                event.preventDefault();
+        if (validatePilot === "Not a Number" && validateCopilot === "Not a Number" && validateFuel === "Is a Number" && validateCargo === "Is a Number") {
+            if (fuelLevel.value < 10000 || cargoLevel.value > 10000) {
+                launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+                launchStatus.style.color = "#C7254E";
+                list.style.visibility = "visible";
+                if (fuelLevel.value < 10000) {
+                    fuelStatus.innerHTML = "Fuel level too low for launch";
+                    event.preventDefault();
+                }
+                if (cargoLevel.value > 10000) {
+                    cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+                }
+            } else {
+                launchStatus.innerHTML = "Shuttle is Ready for Launch";
+                launchStatus.style.color = "#419F6A";
             }
-            if (cargoLevel.value > 10000) {
-                cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-            }
-        } else {
-            launchStatus.innerHTML = "Shuttle is Ready for Launch";
-            launchStatus.style.color = "#419F6A";
             pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch`;
             copilotStatus.innerHTML = `Co-pilot ${copilot.value} is ready for launch`;
             list.style.visibility = "visible";
             event.preventDefault();
+
         }
+       
     });
 }
 
